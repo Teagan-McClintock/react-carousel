f10b35a050180c3b0c59f8142bc0020ceadfc5b8
Crystal Tran <c.t.tran95@gmail.com>
2024-01-16 19:17:03 -0500

    created test for left arrow button on carousel image

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -30,6 +30,40 @@ it("works when you click on the right arrow", function() {
   ).toBeInTheDocument();
 });
 
+
+it("works when you click on the left arrow", function() {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+  // expect the first image to show, but not the second
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+
+  // move backward in the carousel
+  const leftArrow = container.querySelector(".bi-arrow-left-circle");
+  fireEvent.click(leftArrow);
+
+  // expect the first image to show, but not the second if left arrow is clicked
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+});
+
+
 it("renders without crashing", function() {
   render(
     <Carousel
@@ -38,6 +72,7 @@ it("renders without crashing", function() {
     />
   );
 });
+
 
 it("matches snapshot", function() {
   const { container } = render(<Carousel
